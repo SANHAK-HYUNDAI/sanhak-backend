2f2a43f7e5d859d2aebbe65eadb36b62ca352f16
situodys <situodys@gmail.com>
2022-09-13 21:46:30 +0900

    Test: ROMappingPost repository test
    - post 검색 및 페이징 처리, 현재는 cafe 정보만 검색 조건 설정

--- src/main/java/com/sanhak/backend/domain/post/repository/ROMappingPostRepository.java
+++ src/main/java/com/sanhak/backend/domain/post/repository/ROMappingPostRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ROMappingPostRepository extends JpaRepository<ROMappingPost, Long> {
+public interface ROMappingPostRepository extends JpaRepository<ROMappingPost, Long>, ROMappingPostRepositoryDSL {
     void deleteById(Long id);
 }
---
+++ src/main/java/com/sanhak/backend/domain/post/repository/ROMappingPostRepositoryDSLImpl.java
@@ -0,0 +1,73 @@
+package com.sanhak.backend.domain.post.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.util.StringUtils;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sanhak.backend.domain.post.ROMappingPost;
+import com.sanhak.backend.domain.post.dto.PostSearch;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.sanhak.backend.domain.RO.QRepairOrder.*;
+import static com.sanhak.backend.domain.article.QCafeArticle.*;
+import static com.sanhak.backend.domain.post.QROMappingPost.*;
+
+@Repository
+public class ROMappingPostRepositoryDSLImpl extends QuerydslRepositorySupport implements ROMappingPostRepositoryDSL {
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private static final int NEED_CALCULATE = -1;
+
+    public ROMappingPostRepositoryDSLImpl(JPAQueryFactory jpaQueryFactory) {
+        super(ROMappingPost.class);
+        this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    public Page<ROMappingPost> SearchPostWithPagination(PostSearch postSearch) {
+
+        PageRequest pageRequest = PageRequest.of(postSearch.getPage(), postSearch.getSize());
+
+        JPAQuery<ROMappingPost> query = jpaQueryFactory.select(rOMappingPost)
+                .from(rOMappingPost)
+                .leftJoin(rOMappingPost.repairOrder, repairOrder)
+                .fetchJoin()
+                .leftJoin(rOMappingPost.cafeArticle, cafeArticle)
+                .fetchJoin()
+                .where(
+                        containsTitle(postSearch.getTitle()),
+                        containsCafeName(postSearch.getCafeName()),
+                        containsContent(postSearch.getContent()),
+                        containsCategory(postSearch.getCategory())
+                );
+
+        int totalCount = postSearch.getTotalCount() == NEED_CALCULATE ? query.fetch().size() : postSearch.getTotalCount();
+        List<ROMappingPost> result = this.getQuerydsl().applyPagination(pageRequest, query).fetch();
+
+        return new PageImpl<>(result, pageRequest, totalCount);
+    }
+
+    private BooleanExpression containsTitle(String title) {
+        if (StringUtils.isNullOrEmpty(title)) return null;
+        return cafeArticle.title.contains(title);
+    }
+
+    private BooleanExpression containsCafeName(String cafeName) {
+        if (StringUtils.isNullOrEmpty(cafeName)) return null;
+        return cafeArticle.cafeName.contains(cafeName);
+    }
+    private BooleanExpression containsContent(String content) {
+        if (StringUtils.isNullOrEmpty(content)) return null;
+        return cafeArticle.content.contains(content);
+    }
+
+    private BooleanExpression containsCategory(String category) {
+        if (StringUtils.isNullOrEmpty(category)) return null;
+        return cafeArticle.category.contains(category);
+    }
+}
