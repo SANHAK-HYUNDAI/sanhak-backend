111707564358342406c51e1c6e44082934ca751a
situodys <situodys@gmail.com>
2022-12-22 14:42:48 +0900

    feat: basic RO Mvc 구현

---
+++ src/main/java/com/sanhak/backend/domain/RO/dto/request/ROPageRequest.java
@@ -0,0 +1,36 @@
+package com.sanhak.backend.domain.RO.dto.request;
+
+import com.sanhak.backend.global.dto.BasePageRequest;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.PageRequest;
+
+@NoArgsConstructor
+@Setter
+public class ROPageRequest {
+
+    private static final int DEFAULT_SIZE = 5;
+    private static final int DEFAULT_PAGE = 0;
+
+    private Integer page;
+
+    protected Integer size;
+
+    public Integer getPage() {
+        if (this.page == null) {
+            return DEFAULT_PAGE;
+        }
+        return this.page;
+    }
+
+    public Integer getSize() {
+        if (this.size == null) {
+            return DEFAULT_SIZE;
+        }
+        return this.size;
+    }
+
+    public PageRequest getPageRequest() {
+        return PageRequest.of(getPage(), getSize());
+    }
+}
