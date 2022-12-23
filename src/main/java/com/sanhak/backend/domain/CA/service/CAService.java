8c88c378cb9abca1947d6edca1c7d4804a6a1d69
situodys <situodys@gmail.com>
2022-12-23 22:53:07 +0900

    feat(CA): service 구현

---
+++ src/main/java/com/sanhak/backend/domain/CA/service/CAService.java
@@ -0,0 +1,60 @@
+package com.sanhak.backend.domain.CA.service;
+
+import com.sanhak.backend.domain.CA.dto.CAResponseAssembler;
+import com.sanhak.backend.domain.CA.dto.request.CAPageRequest;
+import com.sanhak.backend.domain.CA.dto.response.CAPageResponse;
+import com.sanhak.backend.domain.CA.dto.response.CADetailResponse;
+import com.sanhak.backend.domain.CA.dto.response.CASimpleResponse;
+import com.sanhak.backend.domain.CA.dto.response.CAStatisticsResponse;
+import com.sanhak.backend.domain.CA.entity.CafeArticle;
+import com.sanhak.backend.domain.CA.repository.CARepository;
+import com.sanhak.backend.domain.RO.dto.response.ROResponse;
+import com.sanhak.backend.domain.category.dto.response.CABigCateResponse;
+import com.sanhak.backend.domain.category.dto.response.CASubCateResponse;
+import com.sanhak.backend.domain.category.service.CACategoryService;
+import com.sanhak.backend.domain.keyword.dto.response.KeywordResponse;
+import com.sanhak.backend.domain.keyword.service.KeywordService;
+import com.sanhak.backend.domain.similarity.dto.response.SimilarityResponse;
+import com.sanhak.backend.domain.similarity.entity.Similarity;
+import com.sanhak.backend.domain.similarity.service.SimilarityService;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CAService {
+    private final CARepository caRepository;
+
+    private final SimilarityService similarityService;
+    private final CACategoryService caCategoryService;
+    private final KeywordService keywordService;
+
+    private final CAResponseAssembler responseAssembler;
+
+    public CAPageResponse getSimpleCAsByBigPhenom(CAPageRequest request) {
+        Page<CASimpleResponse> result = similarityService.getSimilaritiesByBigPhenom(request)
+                .map(similarity -> responseAssembler.createCASimpleResponse(similarity.getCafeArticle()));
+
+        return responseAssembler.createCAPageResponse(result);
+    }
+
+    public CADetailResponse getDetailCAById(Long caId) {
+        CafeArticle ca = caRepository.findById(caId)
+                .orElseThrow(EntityNotFoundException::new);
+        List<ROResponse> ros = similarityService.getRepairOrdersByCafeArticle(ca);
+
+        return responseAssembler.createCADetailResponse(ca, ros);
+    }
+
+    public CAStatisticsResponse getCAStatistics() {
+        List<KeywordResponse> keywordResponses = keywordService.getTop50Keywords();
+        List<CASubCateResponse> subCateResponses = caCategoryService.getTop10SubCategories();
+        List<CABigCateResponse> bigCateResponses = caCategoryService.getTop10BigCategories();
+
+        return responseAssembler.createCAStatisticsResponse(keywordResponses, bigCateResponses, subCateResponses);
+    }
+}
