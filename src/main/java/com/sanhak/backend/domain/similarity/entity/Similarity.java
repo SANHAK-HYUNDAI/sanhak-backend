b501f79bc931ea4d7ee1e770ded81761092af359
situodys <situodys@gmail.com>
2022-12-23 23:13:27 +0900

    feat(Similarity): Entity 생성자 추가

--- src/main/java/com/sanhak/backend/domain/similarity/entity/Similarity.java
+++ src/main/java/com/sanhak/backend/domain/similarity/entity/Similarity.java
@@ -3,13 +3,17 @@ package com.sanhak.backend.domain.similarity.entity;
 import com.sanhak.backend.domain.CA.entity.CafeArticle;
 import com.sanhak.backend.domain.RO.entity.RepairOrder;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "similarity")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Similarity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "similarity_id")
     private Long id;
 
@@ -26,4 +30,9 @@ public class Similarity {
             foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT)
     )
     private RepairOrder repairOrder;
+
+    public Similarity(CafeArticle cafeArticle, RepairOrder repairOrder) {
+        this.cafeArticle = cafeArticle;
+        this.repairOrder = repairOrder;
+    }
 }
