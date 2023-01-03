bcbf53e73ce8a9b072b8b4d70058f8575f7b590d
situodys <situodys@gmail.com>
2023-01-03 22:48:37 +0900

    feat(RO): service 구현

--- src/main/java/com/sanhak/backend/domain/RO/service/ROService.java
+++ src/main/java/com/sanhak/backend/domain/RO/service/ROService.java
@@ -1,10 +1,19 @@
 package com.sanhak.backend.domain.RO.service;
 
+import com.sanhak.backend.domain.CA.dto.response.CAStatisticsResponse;
 import com.sanhak.backend.domain.RO.dto.request.ROPageRequest;
 import com.sanhak.backend.domain.RO.dto.response.ROPageResponse;
 import com.sanhak.backend.domain.RO.dto.response.ROResponse;
-import com.sanhak.backend.domain.RO.entity.RepairOrder;
+import com.sanhak.backend.domain.RO.dto.response.ROStatisticsResponse;
 import com.sanhak.backend.domain.RO.repository.RORepository;
+import com.sanhak.backend.domain.category.dto.response.CABigCateResponse;
+import com.sanhak.backend.domain.category.dto.response.CASubCateResponse;
+import com.sanhak.backend.domain.category.dto.response.ROBigCateResponse;
+import com.sanhak.backend.domain.category.dto.response.ROSubCateResponse;
+import com.sanhak.backend.domain.category.service.ROCategoryService;
+import com.sanhak.backend.domain.keyword.dto.response.KeywordResponse;
+import com.sanhak.backend.domain.keyword.service.ROKeywordService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -13,11 +22,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ROService {
 
+    private final ROKeywordService keywordService;
+    private final ROCategoryService roCategoryService;
+
     private final RORepository roRepository;
 
     public ROPageResponse getROs(ROPageRequest roPageRequest) {
         Page<ROResponse> result = roRepository.findAll(roPageRequest.getPageRequest())
                 .map(repairOrder -> new ROResponse(repairOrder));
         return new ROPageResponse(result);
+    }
+
+    public ROStatisticsResponse getROStatistics() {
+        List<KeywordResponse> keywordResponses = keywordService.getTop50Keywords();
+        List<ROSubCateResponse> subCateResponses = roCategoryService.getTop10SubCategories();
+        List<ROBigCateResponse> bigCateResponses = roCategoryService.getTop10BigCategories();
+
+        return new ROStatisticsResponse(keywordResponses, bigCateResponses, subCateResponses);
     }
 }
